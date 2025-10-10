85905387ee0bbad1898e01f758178a551e2a83dc
Anurag choudhary <anurag.choudhary6767@gmail.com>
2025-10-10 13:18:35 +0530

    Add files via upload

---
+++ lect22.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[]="abcabd"
+    char target = 'c'
+    int freq[256]={0};
+    for(int i=0;i<ln;i++){
+        freq[str[i]]++;
+    }
+    for(int i=0; i< ln; i++){
+        if(freq[str[i]]==1){
+            printf("%c",str[i]);
+            break;
+        }
+    }
+
+}
+  printf(%s,   (str));
+
+
