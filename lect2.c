34ab1db31a1d9931bc870872c5fc082f9d251e59
Anurag choudhary <anurag.choudhary6767@gmail.com>
2025-10-16 21:55:19 +0530

    Add files via upload

---
+++ lect2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 20;
+    int num2 = 7;
+    int difference = num1 - num2;
+
+    printf("The difference is: %d\n", difference); // Output: The difference is: 13
+    return 0;
+}
