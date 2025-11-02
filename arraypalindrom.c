bf588d304c293463760d641c6aa66e7be6f0feb1
Anurag choudhary <anurag.choudhary6767@gmail.com>
2025-11-02 16:13:38 +0530

    Add files via upload

---
+++ arraypalindrom.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int arr[5]={1,2,3,2,1}
+int istrue =1;
+for(int i=0;i<5/2;i++)
+if(arr[i]!=arr[5-i-1]){
+   int istrue =0;
+}
+}
+if(istrue==1){
+    printf("haii");
+}else(isfals==0);{
+    printf("nahi haii");
+}
