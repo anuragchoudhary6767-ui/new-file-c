9ed8c9740cceceefd68faf2cf6bde7f9032c4ef4
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-05-24 00:42:46 +0530

    Add files via upload

---
+++ aiou.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{   
+    char str[]="aiouabvs";
+    int count;
+    int cCount=0;
+    int cCount=0;
+    for( int i<0;i<ln;i++)
+    
+        char ch==str[i];
+        {
+         if (str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+         {
+            vCount++;
+         }
+         else
+         {
+            cCount++;
+         }
+        }
+        printf( "%d %d",vCount,cCount)
+};
