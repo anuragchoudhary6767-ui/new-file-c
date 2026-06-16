ca196cfffc670daee3e3a4d26e8ecfdb4efa3c72
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-06-16 15:38:20 +0530

    Add files via upload

---
+++ maxpalindrom.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int arr[5]={2,3,-4,5,-7}
+    int maxsum=arr[0];
+    for(i=0;i<6;i++){
+        int currsum=0;
+        for(int j=i;;j<6;j++){
+            currsum=currsum=arr[j];
+        }
+        if(currsum>maxsum){
+            maxsum=currsum;
+        }
+    }printf("%d",maxsum);
+}
